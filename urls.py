2dcdc35c9f4ab64057430c0115fff4c6f22372e3
Dan <dan.tyan@gmail.com>
2013-01-19 01:15:48 +0600

    fix signup form

--- urls.py
+++ urls.py
@@ -22,6 +22,17 @@ urlpatterns = patterns( '',
         },
         name='signup',
     ),
+    url(
+        r'^signup/done/$',
+        'accounts.views.signup_done',
+        {'template_name': 'accounts/signup_done.html'},
+        name='signup-done',
+    ),
+    url(
+        r'^signup/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        'accounts.views.signup_confirm',
+        name='signup-confirm',
+    ),
 
     ( r'^password_change/$', 'django.contrib.auth.views.password_change', {'template_name': 'accounts/password_change_form.html'} ),
     ( r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'accounts/password_change_done.html'} ),
@@ -32,12 +43,7 @@ urlpatterns = patterns( '',
     ( r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'accounts/password_reset_confirm.html'} ),
     ( r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'accounts/password_reset_complete.html'} ),
 
-    url(
-        r'^signup/done/$',
-        'accounts.views.signup_done',
-        {'template_name': 'accounts/signup_done.html'},
-        name='signup-done',
-    ),
-    ( r'^signup/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'accounts.views.signup_confirm' ),
+
+
     ( r'^signup/complete/$', 'accounts.views.signup_complete', {'template_name': 'accounts/signup_complete.html'} ),
  )
